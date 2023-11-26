b318a6bcba0f1217ed27eeda445dc63feec7f476
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-11-26 16:30:43 +0600

    store users information API created

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb imports
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8snrbzq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // database collections
+    const usersCollection = client.db("arenaContest").collection("users");
+
+    // store users information into database
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already existed" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("ArenaContest server is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
